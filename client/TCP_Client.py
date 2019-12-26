5774d8014341fbdeb5a13e12ec5ba08f69c8a8b8
Reza Tavasoli <jfk998@gmail.com>
2019-12-26 20:09:12 +0330

    Add asymmetric encryption

--- client/TCP_Client.py
+++ client/TCP_Client.py
@@ -1,17 +1,76 @@
 import socket
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
-host_ip, server_port = "127.0.0.1", 9999
-
+host_ip, server_port = "127.0.0.1", 9982
 tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM, )
+
+username = input('Username: ')
+
+
+def generate_asymmetric_keys():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=1024,
+        backend=default_backend()
+    )
+    public_key = private_key.public_key()
+    return public_key, private_key
+
+
+def storing_keys(public_key, private_key):
+    store_private = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+    store_public = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    return store_public, store_private
+
+
+def public_key_encrypt(server_key, message):
+    message = message.encode()
+    public_key = serialization.load_pem_public_key(
+        server_key,
+        backend=default_backend()
+    )
+
+    return public_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
 
 
 try:
+    server_public_key = b''
     tcp_client.connect((host_ip, server_port))
-    tcp_client.sendall(b'user sent data')
+    public, private = generate_asymmetric_keys()
+    rpublic, rprivate = storing_keys(public, private)
 
-    while True:
-        received = tcp_client.recv(1024)
-        print("Bytes Received: {}".format(received.decode()))
+    tcp_client.sendall(b'snd_usrnm' + username.encode() + rpublic)
+    received = tcp_client.recv(1024)
+    if received.startswith(b'server_public'):
+        server_public_key = received[13:]
+
+    print(server_public_key)
+    session_key = input('enter session key: ')
+    print(len(public_key_encrypt(server_public_key, session_key)))
+    print()
+    tcp_client.sendall(b'snd_sekey' + public_key_encrypt(server_public_key, session_key))
+
+    # while True:
+    #     received = tcp_client.recv(1024)
+    #     print("Bytes Received: {}".format(received.decode()))
 except KeyboardInterrupt:
     print('connection closed by user')
 finally:
--- server/TCP-Server.py
+++ server/TCP-Server.py
@@ -1,21 +1,77 @@
 import socketserver
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+
+def generate_asymmetric_keys():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=1024,
+        backend=default_backend()
+    )
+    public_key = private_key.public_key()
+    return public_key, private_key
+
+
+def storing_keys(public_key, private_key):
+    store_private = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+    store_public = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    return store_public, store_private
+
+
+def info_file(username, user_public, server_private):
+    print(user_public)
+    with open('table.secret', 'a+') as f:
+        f.write(f'{username}||||||{user_public}||||||{server_private}')
 
 
 class ServerHandler(socketserver.BaseRequestHandler):
+    public, private = generate_asymmetric_keys()
+    rpublic, rprivate = storing_keys(public, private)
+    username = ''
+    user_public_key = ''
+    action = ''
+    session_key = ''
 
     def handle(self):
         try:
             while True:
+                self.data = b''
                 # self.request - TCP socket connected to the client
                 self.data = self.request.recv(1024).strip()
-                print(self.data)
-                self.request.sendall("ACK from TCP Server".encode())
+                self.action = self.data[:9].decode()
+                if self.action == 'snd_usrnm':
+                    self.username = self.data[9:-271].decode()
+                    self.user_public_key = self.data[-271:].decode()
+                    # info_file(self.username, self.user_public_key, self.rprivate)
+                    self.request.sendall(b'server_public' + self.rpublic)
+                elif self.action == 'snd_sekey':
+                    self.session_key = self.private.decrypt(
+                        self.data[9:],
+                        padding.OAEP(
+                            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                            algorithm=hashes.SHA256(),
+                            label=None
+                        )
+                    )
+                    print(self.session_key)
+
+                # self.request.sendall("ACK from TCP Server".encode())
         except KeyboardInterrupt:
             print('connection closed!')
 
 
-
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "localhost", 9982
     tcp_server = socketserver.TCPServer((HOST, PORT), ServerHandler)
     tcp_server.serve_forever()
